aef04aab6a7da262377549d916cf39c87d2eac83
Shubham Gupta <shubhamguptaji81@gmail.com>
2024-04-22 21:43:02 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 function locationfn() { 
+  loadfn();
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       const latitude = position.coords.latitude;
@@ -17,16 +18,42 @@ function locationfn() {
         .then(data => {
           document.getElementById("weatherPage").innerHTML = data;
         })
+        .then(() => successfn("Fetched successfully."))
         .catch(error => {
-          console.error('Error:', error);
+          errorfn(error.message);
         });
     },
     (error) => {
-      console.log(error);
+      // console.log(error);
+      errorfn(error);
     });
   } else {
     console.error('Geolocation is not supported in this browser');
   }
+}
+function successfn(message){
+  document.getElementById('siteInfo').style.display = 'flex';
+  document.getElementById('siteLoader').style.display = 'none';
+  document.getElementById('siteSuccess').style.display = 'block';
+  document.getElementById('siteMessage').innerHTML = message;
+  new Promise(resolve => setTimeout(()=>{
+    document.getElementById('siteInfo').style.display = 'none';
+    document.getElementById('siteSuccess').style.display = 'none';
+}, 1500));
+}
+function errorfn(message){
+  document.getElementById('siteLoader').style.display = 'none';
+  document.getElementById('siteError').style.display = 'block';
+  document.getElementById('siteMessage').innerHTML = message;
+  new Promise(resolve => setTimeout(()=>{
+      document.getElementById('siteInfo').style.display = 'none';
+      document.getElementById('siteError').style.display = 'none';
+  }, 1500));
+}
+function loadfn(){
+  document.getElementById('siteInfo').style.display = 'flex';
+  document.getElementById('siteLoader').style.display = 'block';
+  document.getElementById('siteMessage').innerHTML = 'Fetching local weather.'
 }
 
  
--- routes/route.js
+++ routes/route.js
@@ -10,31 +10,55 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.get("/", async (req, res) => {
-  const ipAddress = "27.58.192.217" || req.ip;
-  const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${ipAddress}&days=3`);
-  weather.weatherapp(req, res, weatherData.data);
+  try {
+    const ipAddress = req.ip;
+    const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${ipAddress}&days=3`);
+    weather.weatherapp(req, res, weatherData.data); 
+  } catch (error) {
+    const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=49.36.168.72&days=3`);
+    weather.weatherapp(req, res, weatherData.data);
+  }
 });
 
 router.post("/", async (req, res) => {
-  const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${req.body.Address}&days=3`);
-  weather.weatherapp(req, res, weatherData.data);
+  try {
+    const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${req.body.Address}&days=3`);
+    weather.weatherapp(req, res, weatherData.data);
+  } catch (error) {
+    const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=lucknow&days=3`);
+    weather.weatherapp(req, res, weatherData.data);
+  }
 });
 
 router.post('/zcsgtouzewrjbrwq', async (req, res) => {
-  const { latitude, longitude } = req.body;
-  const coordLocation = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${locationApi}`);
-  const [city, cityCountry] = [coordLocation.data[0].name,coordLocation.data[0].country];
-  const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city},${cityCountry}&days=3`);
-  weather.weatherapp(req, res, weatherData.data);
+  try {
+    const { latitude, longitude } = req.body;
+    const coordLocation = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${locationApi}`);
+    const [city, cityCountry] = [coordLocation.data[0].name,coordLocation.data[0].country];
+    const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city},${cityCountry}&days=3`);
+    weather.weatherapp(req, res, weatherData.data);
+  } catch (error) {
+    const weatherData = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=lucknow,India&days=3`);
+    weather.weatherapp(req, res, weatherData.data);
+  }
 });
 
 router.get("/:name", (req, res) => {
-  const header = {
-    title: req.params.name,
-    description: "You have searched for a wrong page.",
-    keyword: "Alcodemy, Wrong Alcodemy"
-  };
-  res.status(404).render("error", { header: header });
+  try {
+    const header = {
+      title: req.params.name,
+      description: "You have searched for a wrong page.",
+      keyword: "Alcodemy, Wrong Alcodemy"
+    };
+    res.status(404).render("error", { header: header });
+  } catch (error) {
+    const header = {
+      title: "Page not found",
+      description: "You have searched for a wrong page.",
+      keyword: "Alcodemy, Wrong Alcodemy"
+    };
+    res.status(404).render("error", { header: header })
+  }
 });
 
 module.exports = router;
